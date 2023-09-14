13c24cbe0650cff0a19c4c22087e046eb1006cf5
Joao-Ale <joao.alexandress12@gmail.com>
2023-09-13 22:38:10 -0300

    First commit

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+// Função para rolar suavemente para as âncoras de navegação
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
