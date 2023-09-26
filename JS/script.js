7e6d1b0fc4577edf9311560095a6b209cf5d58a6
Joao-Ale <joao.alexandress12@gmail.com>
2023-09-26 20:41:15 -0300

    Update project

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 // Função para rolar suavemente para as âncoras de navegação
+// Função para rolar suavemente para as âncoras de navegação
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('a[href^="#"]');
     
@@ -19,27 +20,37 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Função para mostrar uma mensagem de confirmação ao clicar no botão "Entre em Contato"
+const contactButton = document.querySelector(".contact-button");
+
+contactButton.addEventListener("click", function () {
+        alert("Sua mensagem foi enviada!");
+});
+
 const carousel = document.querySelector(".carousel");
-const images = document.querySelectorAll(".image-services");
-const imageWidth = images[0].clientWidth;
+const images = document.querySelectorAll(".gallery-images");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
 let currentIndex = 0;
 
 function moveCarousel(direction) {
-    if (direction === "next") {
-        currentIndex = (currentIndex + 1) % images.length;
-    } else if (direction === "prev") {
-        currentIndex = (currentIndex - 1 + images.length) % images.length;
-    }
-
-    const translateX = -currentIndex * imageWidth;
-    carousel.style.transform = `translateX(${translateX}px)`;
+if (direction === "next") {
+    currentIndex = (currentIndex + 1) % images.length;
+} else if (direction === "prev") {
+    currentIndex =
+    (currentIndex - 1 + images.length) % images.length;
 }
 
-document.querySelector(".next-button").addEventListener("click", () => {
-    moveCarousel("next");
+const translateX = -currentIndex * 100; // 100% de deslocamento horizontal por imagem
+carousel.style.transform = `translateX(${translateX}%)`;
+}
+
+nextButton.addEventListener("click", () => {
+moveCarousel("next");
 });
 
-document.querySelector(".prev-button").addEventListener("click", () => {
-    moveCarousel("prev");
+prevButton.addEventListener("click", () => {
+moveCarousel("prev");
 });
 
